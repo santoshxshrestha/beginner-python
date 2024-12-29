873009485214c92869e807a71b162f8565c2fee6
Santosh <santoshsantoshsthastha@gmail.com>
2024-12-29 22:54:28 +0545

    added .env and saved the key

--- weather_app.py
+++ weather_app.py
@@ -1,28 +1,42 @@
-import requests 
+import os
+
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()  # this line will load the key
+
 
 def get_weather(city, api_key):
-    base_url = 'http://api.openweathermap.org/data/2.5/weather?'
-    complete_url = (f"{base_url}q={city}&appid={api_key}&units=metric")
+    base_url = "http://api.openweathermap.org/data/2.5/weather?"
+    complete_url = f"{base_url}q={city}&appid={api_key}&units=metric"
     response = requests.get(complete_url)
-    weather_data = response.json()
-    
 
-    if weather_data["cod"] != "404":
-        main_data = weather_data["main"]
-        weather_desc = weather_data["weather"][0]["description"]
-        temperature = main_data["temp"]
-        humidity = main_data["humidity"]
-        pressure = main_data["pressure"]
+    if response.status_code == 200:
+        weather_data = response.json()
 
-        print(f"City: {city}")
-        print(f"Temperature: {temperature}°C")
-        print(f"Weather Description: {weather_desc}")
-        print(f"Humidity: {humidity}%")
-        print(f"Pressure: {pressure} hPa")
+        if weather_data["cod"] != "404":
+            main_data = weather_data["main"]
+            weather_desc = weather_data["weather"][0]["description"]
+            temperature = main_data["temp"]
+            humidity = main_data["humidity"]
+            pressure = main_data["pressure"]
+
+            print(f"City: {city}")
+            print(f"Temperature: {temperature}°C")
+            print(f"Weather Description: {weather_desc}")
+            print(f"Humidity: {humidity}%")
+            print(f"Pressure: {pressure} hPa")
+        else:
+            print(f"City {city} not found!")
     else:
-        print(f"City {city} not found!")
+        print("Failed to get weather data. HTTP Status Code:", response.status_code)
+
 
 if __name__ == "__main__":
-    api_key = '08f05e346ce847f630cc2a04c0a464d6'
-    city = input("Enter your city name: ")
-    get_weather(city, api_key)
+    api_key = os.getenv("API_KEY")
+
+    if api_key is None:
+        print("API key is missing. Please set it in the .env file.")
+    else:
+        city = input("Enter your city name: ")
+        get_weather(city, api_key)
