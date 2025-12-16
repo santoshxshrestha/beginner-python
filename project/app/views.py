eaaa3b17c6290d3d9142c8762151df251b159115
santoshxshrestha <santoshxshrestha@gmail.com>
2025-12-16 22:35:56 +0545

    added some more tests

---
+++ project/app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    return HttpResponse("Hello, welcome to the home page!")
+
+def another_home(request):
+    return render(request, "home.html")
