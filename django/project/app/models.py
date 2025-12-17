8ffc8f0da80b5741dbb310cbdda34e02903c0624
santoshxshrestha <santoshxshrestha@gmail.com>
2025-12-17 14:56:17 +0545

    added the migration and db

--- django/project/app/models.py
+++ django/project/app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Meroclass(models.Model):
+    name = models.CharField(max_length=20)
+    age = models.IntegerField()
+    email = models.EmailField(unique=True)
+    address = models.CharField(max_length=100)
