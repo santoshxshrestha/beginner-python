93eb2504707a1f94333f54c777a53da2d1f6b7dc
santoshxshrestha <santoshxshrestha@gmail.com>
2025-12-18 16:02:16 +0545

    modesl

--- django/crud_thinge/crud_app/models.py
+++ django/crud_thinge/crud_app/models.py
@@ -8,3 +8,10 @@ class Cources(models.Model):
     title = models.CharField(max_length=20)
     description = models.TextField()
     price = models.IntegerField()
+
+
+class Form(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    message = models.TextField()
+    password = models.CharField(max_length=20)
