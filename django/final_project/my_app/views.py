f3300031d3e4e07ae01ff15f11bc1b1c33b6edbb
santoshxshrestha <santoshxshrestha@gmail.com>
2025-12-21 22:10:48 +0545

    init index to render data of student

--- django/final_project/my_app/views.py
+++ django/final_project/my_app/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+# importing the Student model to send data to frond en d
+from .models import Student
 
 # Create your views here.
 
 
 # View for the home page
 def home(request):
-    return render(request, 'home.html')
+    data = Student.objects.all()
+    return render(request, "index.html", {"students": data})
