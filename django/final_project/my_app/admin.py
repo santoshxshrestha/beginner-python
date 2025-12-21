1c6f26393cddee9c32c38dc175eef46306403780
santoshxshrestha <santoshxshrestha@gmail.com>
2025-12-21 22:16:51 +0545

    fix issue in front end

--- django/final_project/my_app/admin.py
+++ django/final_project/my_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Student
 
 # Register your models here.
+
+# registering the Students model to make it accessible
+# via the Django admin page
+admin.site.register(Student)
