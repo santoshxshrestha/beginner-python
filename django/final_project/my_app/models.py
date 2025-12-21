2abe806e65830d776a32287a6c2505fe1b4e6e6d
santoshxshrestha <santoshxshrestha@gmail.com>
2025-12-21 21:52:24 +0545

    some comments

--- django/final_project/my_app/models.py
+++ django/final_project/my_app/models.py
@@ -3,6 +3,9 @@ from django.db import models
 # Create your models here.
 
 
+# here student id is primary key by default so it will be
+# generate automatically and
+# will be unique for each student
 class Student(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
